b47c0bf1833aec5b9b6028c4baf86f8c616f0816
Gabriel Tidy <gabriel@bbd.co.za>
2023-09-28 09:37:18 +0200

    added tests for negatives

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -4,11 +4,7 @@ describe("Calc-U-Later", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("opens with zero", () => {
-    cy.get("[data-test='display']").should("exist");
-  });
-
-  it("should add correctly", () => {
+  it("should add positives correctly", () => {
     cy.get("[data-test='one']").should("exist").click();
     cy.get("[data-test='add']").should("exist").click();
     cy.get("[data-test='one']").should("exist").click();
@@ -16,7 +12,7 @@ describe("Calc-U-Later", () => {
     cy.get("[data-test='display']").should("exist").should('have.value', '2');
   });
 
-  it("should subtract correctly", () => {
+  it("should subtract positives correctly", () => {
     cy.get("[data-test='five']").should("exist").click();
     cy.get("[data-test='subtract']").should("exist").click();
     cy.get("[data-test='one']").should("exist").click();
@@ -24,7 +20,7 @@ describe("Calc-U-Later", () => {
     cy.get("[data-test='display']").should("exist").should('have.value', '4');
   });
 
-  it("should multiply correctly", () => {
+  it("should multiply positives correctly", () => {
     cy.get("[data-test='five']").should("exist").click();
     cy.get("[data-test='multiply']").should("exist").click();
     cy.get("[data-test='five']").should("exist").click();
@@ -32,9 +28,49 @@ describe("Calc-U-Later", () => {
     cy.get("[data-test='display']").should("exist").should('have.value', '25');
   });
 
-  it("should divide correctly", () => {
+  it("should divide positives correctly", () => {
     cy.get("[data-test='two']").should("exist").click();
     cy.get("[data-test='divide']").should("exist").click();
+    cy.get("[data-test='eight']").should("exist").click();
+    cy.get("[data-test='equals']").should("exist").click();
+    cy.get("[data-test='display']").should("exist").should('have.value', '0.25');
+  });
+
+  it("should add negatives correctly", () => {
+    cy.get("[data-test='subtract']").should("exist").click();
+    cy.get("[data-test='one']").should("exist").click();
+    cy.get("[data-test='add']").should("exist").click();
+    cy.get("[data-test='subtract']").should("exist").click();
+    cy.get("[data-test='one']").should("exist").click();
+    cy.get("[data-test='equals']").should("exist").click();
+    cy.get("[data-test='display']").should("exist").should('have.value', '-2');
+  });
+
+  it("should subtract negatives correctly", () => {
+    cy.get("[data-test='subtract']").should("exist").click();
+    cy.get("[data-test='five']").should("exist").click();
+    cy.get("[data-test='subtract']").should("exist").click();
+    cy.get("[data-test='subtract']").should("exist").click();
+    cy.get("[data-test='one']").should("exist").click();
+    cy.get("[data-test='equals']").should("exist").click();
+    cy.get("[data-test='display']").should("exist").should('have.value', '-4');
+  });
+
+  it("should multiply negatives correctly", () => {
+    cy.get("[data-test='subtract']").should("exist").click();
+    cy.get("[data-test='five']").should("exist").click();
+    cy.get("[data-test='multiply']").should("exist").click();
+    cy.get("[data-test='subtract']").should("exist").click();
+    cy.get("[data-test='five']").should("exist").click();
+    cy.get("[data-test='equals']").should("exist").click();
+    cy.get("[data-test='display']").should("exist").should('have.value', '25');
+  });
+
+  it("should divide negatives correctly", () => {
+    cy.get("[data-test='subtract']").should("exist").click();
+    cy.get("[data-test='two']").should("exist").click();
+    cy.get("[data-test='divide']").should("exist").click();
+    cy.get("[data-test='subtract']").should("exist").click();
     cy.get("[data-test='eight']").should("exist").click();
     cy.get("[data-test='equals']").should("exist").click();
     cy.get("[data-test='display']").should("exist").should('have.value', '0.25');
